97f264a0a5c683375aa5a1ff15d4cdd078e62514
Ameena-Juhi <141002488+Ameena-Juhi@users.noreply.github.com>
2023-08-23 18:01:10 +0530

    CardGameServlet1.java

--- CardGameServlet.java
+++ CardGameServlet.java
@@ -2,6 +2,8 @@ package com.learning.hello;
 
 import java.io.IOException;
 
+import com.learning.hello.controller.CardGame;
+
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -21,12 +23,14 @@ public class CardGameServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 	
+	private CardGame game;
 	private JakartaServletWebApplication application;
 	private TemplateEngine templateEngine;
 	
 	@Override
 	  public void init(ServletConfig config) throws ServletException {
 	    super.init(config);
+	    game = new CardGame();
 	    
 	    application = JakartaServletWebApplication.buildApplication(getServletContext());
 	    final WebApplicationTemplateResolver templateResolver = 
@@ -41,15 +45,33 @@ public class CardGameServlet extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
-		String inp = req.getParameter("card");
-		System.out.println(inp);
+		String input = req.getParameter("card");
+		String Pile = req.getParameter("pile");
+		String pileName = "";
+			if(Pile.equals("in")) {
+				pileName = "InPile";
+			}
+			else if(Pile.equals("out")){
+				pileName = "OutPile";
+			}
+			
+		System.out.println(input);
+		System.out.println(pileName);
+		
 		var out = resp.getWriter();
 		final IWebExchange webExchange = 
 		        this.application.buildExchange(req, resp);
 		    final WebContext ctx = new WebContext(webExchange);
-		    ctx.setVariable("reading", inp );  
+		    
+		    
+		    String list = game.distributeCards();
+		    System.out.println(list);
+		    //out.println(list);
+		    ctx.setVariable("reading",game.verifyCardPosition(input, pileName) );  
+		    ctx.setVariable("inpile",game.getInPile() );  
+		    ctx.setVariable("outpile",game.getOutPile());  
 		    templateEngine.process("Mankatha", ctx, out);
-		
+		    resp.sendRedirect(req.getServletPath());
 	}
 	
 	@Override
